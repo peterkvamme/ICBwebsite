c63186f2bc13ad1d0f0f15d7203b8a8245a47202
peterkvamme <peterkvamme@gmail.com>
2026-07-05 08:59:27 -0500

    Update and rename firebase-config.example.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBKihTX3k-Sp2xJvtBkRm6Ger_lkTGAHYA",
+  authDomain: "ice-cream-boat-d2650.firebaseapp.com",
+  databaseURL: "https://ice-cream-boat-d2650-default-rtdb.firebaseio.com",
+  projectId: "ice-cream-boat-d2650",
+  storageBucket: "ice-cream-boat-d2650.firebasestorage.app",
+  messagingSenderId: "1015342157676",
+  appId: "1:1015342157676:web:2d6a47ecf8b3c369b1ad49"
+};
